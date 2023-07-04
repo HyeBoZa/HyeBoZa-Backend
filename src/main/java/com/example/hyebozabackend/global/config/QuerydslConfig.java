aac6040f3465d2aee974df7e51c17a4f50e1efef
문정민 <moondeve41@gmail.com>
2023-07-04 16:12:41 +0900

    EDIT :: Cors, querydsl config

--- src/main/java/com/example/hyebozabackend/global/config/QuerydslConfig.java
+++ src/main/java/com/example/hyebozabackend/global/config/QuerydslConfig.java
@@ -1,4 +1,19 @@
 package com.example.hyebozabackend.global.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@RequiredArgsConstructor
+@Configuration
 public class QuerydslConfig {
+
+    private final EntityManager entityManager;
+
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
 }
--- src/main/java/com/example/hyebozabackend/global/config/WebMvcConfig.java
+++ src/main/java/com/example/hyebozabackend/global/config/WebMvcConfig.java
@@ -1,4 +1,18 @@
 package com.example.hyebozabackend.global.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry
+                .addMapping("/**")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD")
+                .allowedHeaders("*")
+                .allowedOrigins("*");
+    }
 }
